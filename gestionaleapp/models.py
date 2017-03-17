3e6ca3c397927c9e0d54199c753bbb7f20f4f992
Ettore Forigo <etty.2000.ef@gmail.com>
2017-03-17 22:19:39 +0100

    Started development of database models

--- gestionaleapp/models.py
+++ gestionaleapp/models.py
@@ -1,3 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Person(models.Model):
+	name = models.CharField(max_length=32)
+	surname = models.CharField(max_length=32)
+	born = models.DateTimeField("born date")
+	cap = models.IntegerField()
+	telephone = models.IntegerField()
+	email = models.CharField(max_length=254)
+	tessera = models.CharField(max_length=128)
+
+	def __str__(self):
+		return self.name.capitalize() + ' ' + self.surname.capitalize()
+
+
+class Event(models.Model):
+	participant = models.ForeignKey(Person)
+	date = models.DateTimeField("event date")
+	cost = models.IntegerField()
+	description = models.CharField(max_length=64)		
