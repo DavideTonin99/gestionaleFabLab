5e25a9e913e5f8c6c271964a3dd34b75c1233d66
Ettore Forigo <etty.2000.ef@gmail.com>
2017-03-17 17:42:25 +0100

    Restarted project due to apps problems

--- gestionaleFabLab/urls.py
+++ gestionaleFabLab/urls.py
@@ -1,4 +1,4 @@
-"""gestionale URL Configuration
+"""gestionaleFabLab URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^gestionale/', include('gestionaleapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- gestionaleFabLab/wsgi.py
+++ gestionaleFabLab/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gestionale project.
+WSGI config for gestionaleFabLab project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestionale.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestionaleFabLab.settings")
 
 application = get_wsgi_application()
