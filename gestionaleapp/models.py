2bbb0cc9e407f15145b04caa8f38aa1579c5bad3
Ettore Forigo <etty.2000.ef@gmail.com>
2017-03-20 17:49:09 +0100

    Finished database (to test)

--- gestionaleapp/models.py
+++ gestionaleapp/models.py
@@ -1,26 +1,39 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
 
 
 class Person(models.Model):
-	name = models.CharField(max_length=32)
-	surname = models.CharField(max_length=32)
+	name = models.CharField(max_length=50)
+	surname = models.CharField(max_length=50)
 	born = models.DateTimeField("born date")
-	cap = models.IntegerField()
-	telephone = models.IntegerField()
+	cap = models.DecimalField(max_digits=5, decimal_places=5)
+	telephone = models.DecimalField(max_digits=15)
 	email = models.CharField(max_length=254)
-	tessera = models.CharField(max_length=128)
+	card = models.CharField(max_length=128)
 
 	def __str__(self):
 		return self.name.capitalize() + ' ' + self.surname.capitalize()
 
 
 class Event(models.Model):
-	participant = models.ForeignKey(Person)
+	participant = models.ManyToManyField(Person, null=True)
+	name = models.CharField(max_length=200)
 	date = models.DateTimeField("event date")
 	cost = models.IntegerField()
-	description = models.CharField(max_length=64)		
+	description = models.CharField(max_length=1000)
+
+
+class Subscription(models.Model):
+	person = models.ForeignKey(Person)
+	year = models.PositiveSmallIntegerField()
+	type = models.CharField(max_length=10)
+	occasion = models.ForeignKey(Event, null=True)
+
+
+class Processing(models.Model):
+	type = models.CharField(max_length=100)
+	data = models.DateTimeField("processing date")
+	cost = models.PositiveSmallIntegerField()
+	description = models.CharField(max_length=1000)
+	person = models.ManyToManyField(Person, null=True)
