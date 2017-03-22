bd1878e203f4de89422fcd8c4d830d80a64a9b7a
DavideTonin99 <davide9935@gmail.com>
2017-03-22 17:16:14 +0100

    Added script for page selection in the navbar

--- gestionaleapp/urls.py
+++ gestionaleapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'gestionale'
 urlpatterns = [
     url(r'^anagrafica/$', views.anagrafica),
+    url(r'^eventi/$', views.eventi),
+    url(r'^lavorazioni/$', views.lavorazioni),
 ]
--- gestionaleapp/views.py
+++ gestionaleapp/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 
 from .models import Person
 
+
 # Create your views here.
 
 
 def anagrafica(request):
-	context = {
-		'clients': Person.objects.all()
-	}
-	return render(request, 'gestionaleapp/anagrafica.html', context)
+    context = {
+        'clients': Person.objects.all()
+    }
+    return render(request, 'gestionaleapp/anagrafica.html', context)
+
+
+def eventi(request):
+    context = {
+        'clients': Person.objects.all()
+    }
+    return render(request, 'gestionaleapp/eventi.html', context)
+
+
+def lavorazioni(request):
+    context = {
+        'clients': Person.objects.all()
+    }
+    return render(request, 'gestionaleapp/lavorazioni.html', context)
