d85aa127707cfa1e0529759983bbb8d5c58b8a9e
Ettore Forigo <etty.2000.ef@gmail.com>
2017-03-27 16:55:46 +0200

    Added handling of client data request

--- gestionaleapp/views.py
+++ gestionaleapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Person
@@ -8,24 +9,43 @@ from .models import Person
 
 
 @login_required
-def anagrafica(request):
-    context = {
-        'clients': Person.objects.all()
-    }
-    return render(request, 'gestionaleapp/anagrafica.html', context)
+def customers(request):
+	context = {
+		'clients': Person.objects.all()
+	}
+	return render(request, 'gestionaleapp/anagrafica.html', context)
 
 
 @login_required
-def eventi(request):
-    context = {
-        'clients': Person.objects.all()
-    }
-    return render(request, 'gestionaleapp/eventi.html', context)
+def events(request):
+	context = {
+		'clients': Person.objects.all()
+	}
+	return render(request, 'gestionaleapp/eventi.html', context)
 
 
 @login_required
-def lavorazioni(request):
-    context = {
-        'clients': Person.objects.all()
-    }
-    return render(request, 'gestionaleapp/lavorazioni.html', context)
+def processings(request):
+	context = {
+		'clients': Person.objects.all()
+	}
+	return render(request, 'gestionaleapp/lavorazioni.html', context)
+
+
+def get_client_data(request):
+	if request.method == 'GET':
+		id_ = request.GET.get('id', False)
+		if not id_:
+			return HttpResponseBadRequest(request)
+
+		client = get_object_or_404(Person.objects.filter(id=id_))
+		data = {
+			'card': client.card,
+			'surname': client.surname,
+			'name': client.name,
+			'born': client.born,
+			'cap': client.cap,
+			'telephone': client.telephone,
+			'email': client.email
+		}
+		return JsonResponse(data)
