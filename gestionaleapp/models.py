2f246248c85e3a0c80f067ff24c39370c553b2ca
Ettore Forigo <etty.2000.ef@gmail.com>
2017-04-19 16:36:42 +0200

    Slightly changes to forms and models

--- gestionaleapp/models.py
+++ gestionaleapp/models.py
@@ -10,7 +10,7 @@ class Person(models.Model):
 	cap = models.CharField(max_length=5)
 	telephone = models.CharField(max_length=15)
 	email = models.CharField(max_length=254)
-	card = models.CharField(max_length=128)
+	card = models.CharField(max_length=128, unique=True)
 
 	def __str__(self):
 		return self.name.capitalize() + ' ' + self.surname.capitalize()
