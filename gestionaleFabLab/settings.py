b4827656895f2bd5c8093517cea0fba88b9f9ffb
DavideTonin99 <davide9935@gmail.com>
2017-04-22 21:13:53 +0200

    some modifies

--- gestionaleFabLab/settings.py
+++ gestionaleFabLab/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from decouple import config
-import dj_databse_config
+import dj_database_config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
