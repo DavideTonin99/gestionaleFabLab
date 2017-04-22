80d71d3265d0d7e6d33d59b9b799e420a049b4a2
DavideTonin99 <davide9935@gmail.com>
2017-04-22 20:55:42 +0200

    Added whitenoise to deploy on heroku

--- gestionaleFabLab/settings.py
+++ gestionaleFabLab/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'login'
