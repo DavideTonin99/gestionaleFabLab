9fa4880331add4cfbbbe5f1241d429b70f756c83
DavideTonin99 <davide9935@gmail.com>
2017-06-13 17:38:25 +0200

    Create settings.py

--- gestionaleFabLab/settings.py
+++ gestionaleFabLab/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'xxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 """
 SECURE_HSTS_SECONDS = 31536000
