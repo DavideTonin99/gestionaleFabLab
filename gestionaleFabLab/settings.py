9ccb7e6de5490d54ef6234ac18d6143c355b48f3
DavideTonin99 <davide9935@gmail.com>
2017-06-13 17:53:04 +0200

    Create settings.py

--- gestionaleFabLab/settings.py
+++ gestionaleFabLab/settings.py
@@ -148,9 +148,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'gestionaleapp/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'login'
