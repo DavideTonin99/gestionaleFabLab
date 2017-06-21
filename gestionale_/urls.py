6c2b143bdf80e31f3b7f8dfb8fec55b789c87718
Ettore Forigo <etty.2000.ef@gmail.com>
2017-06-22 01:57:48 +0200

    Anagrafica completely working, model improvements

--- gestionale_/urls.py
+++ gestionale_/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import CreateCustomerView
+from .views import CreateCustomerView, UpdateCustomerView
 
 app_name = 'gestionale_'
 urlpatterns = [
-    url(r'^anagrafica/crea/cliente$', CreateCustomerView.as_view(), name="create_customer"),
+	url(r'^anagrafica/crea/cliente$', CreateCustomerView.as_view(), name="create_customer"),
+	url(r'^anagrafica/modifica/cliente/(?P<customer_id>[0-9]+)/$', UpdateCustomerView.as_view(), name="update_customer")
 ]
--- gestionale_/views.py
+++ gestionale_/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import reverse
+from django.shortcuts import reverse, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView
 
 from .models import Customer
 from .forms import CustomerForm
@@ -15,5 +15,28 @@ class CreateCustomerView(LoginRequiredMixin, CreateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(CreateCustomerView, self).get_context_data(**kwargs)
-		context['customers'] = Customer.objects.all()
+		context['customers'] = self.model.objects.all()
+		context['op'] = 'Crea'
+		return context
+
+
+class UpdateCustomerView(LoginRequiredMixin, UpdateView):
+	model = Customer
+	form_class = CustomerForm
+
+	def get_success_url(self):
+		return reverse('gestionale_:update_customer', args=(self.kwargs.get('customer_id'),))
+
+	def get_object(self, queryset=None):
+		return get_object_or_404(self.model.objects.filter(id=self.kwargs.get('customer_id')))
+
+	def get_context_data(self, **kwargs):
+		context = super(UpdateCustomerView, self).get_context_data(**kwargs)
+		context['customers'] = self.model.objects.all()
+		context['op'] = 'Modifica'
+
+		all_subs = self.object.subscription_set.all()
+		context['subscriptions'] = {year: all_subs.filter(year__year=year) for year in range(2014, 2028 + 1)}
+
+		context['processings'] = self.object.processing_set.all()
 		return context
