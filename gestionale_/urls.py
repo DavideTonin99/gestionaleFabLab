63e5680d22d2d8e3ec5aac8b5f1f7aca2a7c3204
Ettore Forigo <etty.2000.ef@gmail.com>
2017-06-22 22:01:04 +0200

    Started devpmnt of sub form, minor improvements

--- gestionale_/urls.py
+++ gestionale_/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from .views import CreateCustomerView, UpdateCustomerView
+from .views import CreateCustomerView, UpdateCustomerView, CreateSubscriptionView
 
 app_name = 'gestionale_'
 urlpatterns = [
-	url(r'^anagrafica/crea/cliente$', CreateCustomerView.as_view(), name="create_customer"),
-	url(r'^anagrafica/modifica/cliente/(?P<customer_id>[0-9]+)/$', UpdateCustomerView.as_view(), name="update_customer")
+	url(r'^anagrafica/crea/$', CreateCustomerView.as_view(), name='create_customer'),
+	url(r'^anagrafica/modifica/(?P<customer_id>[0-9]+)/$', UpdateCustomerView.as_view(),
+	    name='update_customer'),
+	url(r'^anagrafica/(?P<customer_id>[0-9]+)/crea/(?:(?P<year>[0-9]{4})/)?$', CreateSubscriptionView.as_view(),
+	    name='create_subscription'),
 ]
--- gestionale_/views.py
+++ gestionale_/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import reverse, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, UpdateView
 
-from .models import Customer
-from .forms import CustomerForm
+from .models import Customer, Subscription
+from .forms import CustomerForm, SubscriptionForm
 
 
 class CreateCustomerView(LoginRequiredMixin, CreateView):
@@ -11,7 +11,7 @@ class CreateCustomerView(LoginRequiredMixin, CreateView):
 	form_class = CustomerForm
 
 	def get_success_url(self):
-		return reverse('gestionale_:create_customer')
+		return reverse('gestionale_:update_customer', args=(self.object.id,))
 
 	def get_context_data(self, **kwargs):
 		context = super(CreateCustomerView, self).get_context_data(**kwargs)
@@ -34,9 +34,40 @@ class UpdateCustomerView(LoginRequiredMixin, UpdateView):
 		context = super(UpdateCustomerView, self).get_context_data(**kwargs)
 		context['customers'] = self.model.objects.all()
 		context['op'] = 'Modifica'
+		context['id'] = self.object.id
 
 		all_subs = self.object.subscription_set.all()
+		context['show_tables'] = True
 		context['subscriptions'] = {year: all_subs.filter(year__year=year) for year in range(2014, 2028 + 1)}
 
 		context['processings'] = self.object.processing_set.all()
+		return context
+
+
+class CreateSubscriptionView(LoginRequiredMixin, CreateView):
+	model = Subscription
+	form_class = SubscriptionForm
+
+	def get_customer(self):
+		return Customer.objects.filter(id=self.kwargs.get('customer_id')).get()
+
+	def get_initial(self):
+		initial = super(CreateSubscriptionView, self).get_initial()
+		initial['customer'] = self.get_customer()
+		return initial
+
+	def get_success_url(self):
+		return reverse('gestionale_:update_subscription', args=(self.object.customer.id, self.object.id))
+
+	def get_context_data(self, **kwargs):
+		context = super(CreateSubscriptionView, self).get_context_data(**kwargs)
+		context['op'] = 'Crea'
+
+		customer = self.get_customer()
+		context['id'] = customer.id
+
+		all_subs = customer.subscription_set.all()
+		context['show_tables'] = True
+		context['subscriptions'] = {year: all_subs.filter(year__year=year) for year in range(2014, 2028 + 1)}
+
 		return context
