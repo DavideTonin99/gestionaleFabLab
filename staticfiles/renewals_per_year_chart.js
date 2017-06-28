a46f53b7104e2ad615160c5e17198adfa20e0aad
DavideTonin99 <davide9935@gmail.com>
2017-06-28 21:55:49 +0200

    Aggiunto grafico dei rinnovi per anno

--- staticfiles/renewals_per_year_chart.js
+++ staticfiles/renewals_per_year_chart.js
@@ -1,41 +1,41 @@
-function draw_associated_yearly_chart(data) {
-    var associated_yearly_canvas = document.getElementById('associated_yearly').getContext("2d");
+function draw_renewals_per_year_chart(data) {
+    var renewals_per_year_canvas = document.getElementById('renewals').getContext("2d");
 
     var years = [];
-    var baseDataset;
-    var makerDataset;
+    var renewDataset;
+    var notRenewDataset;
 
     $(data.categories).each(function (index, year) {
         years.push(year);
     });
 
-    baseDataset = {
+    renewDataset = {
         label: data.series[0].name,
         data: data.series[0].data,
-        backgroundColor: colors[0],
-        hoverBackgroundColor: colors[0],
-        hoverBorderWidth: 2,
-        hoverBorderColor: border_colors[0]
-    };
-
-    makerDataset = {
-        label: data.series[1].name,
-        data: data.series[1].data,
         backgroundColor: colors[1],
         hoverBackgroundColor: colors[1],
         hoverBorderWidth: 2,
         hoverBorderColor: border_colors[1]
     };
 
-    window.associated_yearly_chart = new Chart(associated_yearly_canvas, {
+    notRenewDataset = {
+        label: data.series[1].name,
+        data: data.series[1].data,
+        backgroundColor: colors[0],
+        hoverBackgroundColor: colors[0],
+        hoverBorderWidth: 2,
+        hoverBorderColor: border_colors[0]
+    };
+
+    window.renewals_per_year_chart = new Chart(renewals_per_year_canvas, {
             type: 'bar',
             responsive: true,
 
             data: {
                 labels: years,
                 datasets: [
-                    baseDataset,
-                    makerDataset
+                    renewDataset,
+                    notRenewDataset
                 ]
             },
             options: {
