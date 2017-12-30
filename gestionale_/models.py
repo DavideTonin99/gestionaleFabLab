eac4e0311c90b730664272a7ac52863085466d65
Ettore Forigo <etty.2000.ef@gmail.com>
2017-12-31 00:55:33 +0100

    Updated model to new requirements

--- gestionale_/models.py
+++ gestionale_/models.py
@@ -1,20 +1,18 @@
 from datetime import date
+from decimal import Decimal
 
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator, MinValueValidator
 from django.db import models
 
-CARD_PREFIX = 'VRFL'
-
-SUBSCRIPTION_TYPE_CHOICES = ((0, 'Base'), (1, 'Maker'))
-SUBSCRIPTION_PAYMENT_CHOICES = ((0, 'Paypal'), (1, 'Bonifico'), (2, 'Contanti'))
-PROCESSING_TYPE_CHOICES = ((0, 'Laser'), (1, 'Stampa 3D'), (2, 'Fresa'))
-
-cap_regex = RegexValidator(regex=r'^\d{5}$', message='CAP non valido')
-card_regex = RegexValidator(regex=r'^VRFL\d{5}$', message='Tessera non valida')
-phone_regex = RegexValidator(regex=r'^\+?\d{8,15}$', message='Numero di telefono non valido')
-
 
 class Customer(models.Model):
+	CARD_PREFIX = 'VRFL'
+
+	cap_regex = RegexValidator(regex=r'^\d{5}$', message='CAP non valido')
+	card_regex = RegexValidator(regex=r'^VRFL\d{5}$', message='Tessera non valida')
+	phone_regex = RegexValidator(regex=r'^\+?\d{8,15}$', message='Numero di telefono non valido')
+
 	class Meta:
 		verbose_name = 'Cliente'
 		verbose_name_plural = 'Clienti'
@@ -23,11 +21,13 @@ class Customer(models.Model):
 	surname = models.CharField(verbose_name='Cognome', max_length=50)
 	born = models.DateField(verbose_name='Data di nascita', null=True)
 	cap = models.CharField(verbose_name='CAP', validators=[cap_regex], max_length=5, null=True)
-	telephone = models.CharField(verbose_name='Cellulare', default='+39', validators=[phone_regex], max_length=16)
-	email = models.EmailField(null=True, blank=True)
+	telephone = models.CharField(verbose_name='Cellulare', default='+39', validators=[phone_regex], max_length=16,
+	                             null=True)
+	email = models.EmailField(null=True)
 	card = models.CharField(verbose_name='Tessera', default=CARD_PREFIX, validators=[card_regex], max_length=9,
 	                        unique=True)
 	first_association = models.DateField(verbose_name='Data associazione', default=date.today)
+	card_given = models.NullBooleanField(verbose_name='Consegnata tessera')
 
 	def __str__(self):
 		return ' '.join((self.card, self.name.capitalize(), self.surname.capitalize()))
@@ -42,8 +42,8 @@ class Event(models.Model):
 	name = models.CharField(verbose_name='Nome', max_length=200)
 	date = models.DateField(verbose_name='Data', default=date.today)
 	duration = models.PositiveSmallIntegerField(verbose_name='Durata (ore)')
-	price = models.DecimalField(verbose_name='Prezzo', max_digits=6, decimal_places=2,
-	                            validators=[MinValueValidator(0)], null=True, blank=True)
+	price = models.DecimalField(verbose_name='Prezzo', default=Decimal(0), max_digits=6, decimal_places=2,
+	                            validators=[MinValueValidator(0)])
 	description = models.CharField(verbose_name='Descrizione', max_length=1000, null=True, blank=True)
 
 	def __str__(self):
@@ -51,30 +51,46 @@ class Event(models.Model):
 
 
 class Subscription(models.Model):
+	TYPE_CHOICES = enumerate(('Base', 'Maker'))
+	PAYMENT_CHOICES = enumerate(('Paypal', 'Bonifico', 'Contanti'))
+
 	class Meta:
 		verbose_name = 'Iscrizione'
 		verbose_name_plural = 'Iscrizioni'
 
-	customer = models.ForeignKey(Customer, verbose_name='Cliente', unique_for_year='year', on_delete=models.CASCADE)
-	date = models.DateField(verbose_name='Data', default=date.today, validators=[])
-	type = models.PositiveSmallIntegerField(verbose_name='Tipo', choices=SUBSCRIPTION_TYPE_CHOICES)
-	payment = models.PositiveSmallIntegerField(verbose_name='Pagamento', choices=SUBSCRIPTION_PAYMENT_CHOICES)
+	customer = models.ForeignKey(Customer, verbose_name='Cliente', on_delete=models.CASCADE)
+	start_date = models.DateField(verbose_name='Data Inizio', default=date.today, validators=[])
+	end_date = models.DateField(verbose_name='Data Fine', default=date.today, validators=[])
+	type = models.PositiveSmallIntegerField(verbose_name='Tipo', choices=TYPE_CHOICES)
+	payment = models.PositiveSmallIntegerField(verbose_name='Pagamento', choices=PAYMENT_CHOICES)
 	occasion = models.ForeignKey(Event, verbose_name='Occasione', null=True, blank=True, on_delete=models.SET_NULL)
 
+	def clean(self):
+		if Subscription.objects.filter(customer=self.customer,
+		                               start_date__lte=self.end_date,
+		                               end_date__gte=self.start_date).exists():
+			raise ValidationError('Iscrizione per questo periodo già esistente')
+
+		super(Subscription, self).clean()
+
 	def __str__(self):
-		return ', '.join(('Iscrizione', self.customer.name, str(self.date.year)))
+		return ', '.join(('Iscrizione', self.customer.name, str(self.start_date.year)))
 
 
 class Processing(models.Model):
+	TYPE_CHOICES = enumerate(('Laser', 'Stampa 3D', 'Fresa'))
+
 	class Meta:
 		verbose_name = 'Lavorazione'
 		verbose_name_plural = 'Lavorazioni'
 
 	date = models.DateField(verbose_name='Data', default=date.today)
 	customer = models.ForeignKey(Customer, verbose_name='Cliente', on_delete=models.CASCADE)
-	type = models.PositiveSmallIntegerField(verbose_name='Tipo', choices=PROCESSING_TYPE_CHOICES)
-	price = models.DecimalField(verbose_name='Prezzo', max_digits=6, decimal_places=2,
-	                            validators=[MinValueValidator(0)], null=True, blank=True)
+	type = models.PositiveSmallIntegerField(verbose_name='Tipo', choices=TYPE_CHOICES)
+	materials_price = models.DecimalField(verbose_name='Prezzo materiali', max_digits=6, decimal_places=2,
+	                                      validators=[MinValueValidator(0)], null=True, blank=True)
+	machinery_price = models.DecimalField(verbose_name='Prezzo macchinari', max_digits=6, decimal_places=2,
+	                                      validators=[MinValueValidator(0)], null=True, blank=True)
 	description = models.CharField(verbose_name='Descrizione', max_length=1000, null=True, blank=True)
 
 	def __str__(self):
